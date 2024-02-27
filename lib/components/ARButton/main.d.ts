7eb72b5af113035124181c5ea9f41a893cfac596
Matej Drlicka <sprattek@gmail.com>
2024-02-27 15:19:40 +0100

    published

--- lib/components/ARButton/main.d.ts
+++ lib/components/ARButton/main.d.ts
@@ -7,6 +7,6 @@ export interface Options {
     stopText?: string;
 }
 declare class ARButton {
-    static createButton(options?: Options): Promise<HTMLAnchorElement | HTMLButtonElement>;
+    static createButton(options?: Options): Promise<HTMLButtonElement | HTMLAnchorElement>;
 }
 export { ARButton };
